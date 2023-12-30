22e4b78a812f9a25106294453c12ac6141c9e303
RuiwenTang <tangruiwen1989@gmail.com>
2023-12-31 00:01:30 +0800

    Fix vulkan symbol conflict

--- Engine/src/render/vk/render_system_vk.cc
+++ Engine/src/render/vk/render_system_vk.cc
@@ -1,5 +1,7 @@
 #include "render/vk/render_system_vk.h"
 
+#include <cubic/core/log.h>
+
 namespace cubic {
 
 std::unique_ptr<RenderSystemVk> RenderSystemVk::Create() { return std::make_unique<RenderSystemVk>(); }
@@ -10,6 +12,32 @@ RenderSystemVk::~RenderSystemVk() {
   }
 }
 
-bool RenderSystemVk::Init() { return false; }
+bool RenderSystemVk::Init() {
+  if (volkInitialize() != VK_SUCCESS) {
+    CUB_ERROR("Failed load vulkan driver !!");
+    return false;
+  }
+
+  VkApplicationInfo app_info{};
+  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  app_info.pEngineName = "Cubic Engine";
+  app_info.pApplicationName = "Cubic";
+  app_info.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+  app_info.apiVersion = VK_API_VERSION_1_3;
+
+  VkInstanceCreateInfo instance_desc{};
+  instance_desc.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  instance_desc.pApplicationInfo = &app_info;
+
+  if (vkCreateInstance(&instance_desc, nullptr, &mInstance) != VK_SUCCESS) {
+    CUB_ERROR("Failed create vulkan instance !!");
+
+    return false;
+  }
+
+  volkLoadInstance(mInstance);
+
+  return true;
+}
 
 }  // namespace cubic
